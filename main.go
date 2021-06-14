d0d02a466e81553f4635d3938709b524bb816b31
tommt <atombombmount@gmail.com>
2021-06-14 16:23:05 +0545

    404 status page

--- main.go
+++ main.go
@@ -5,12 +5,15 @@ import "net/http"
 
 func handlerFunc(w http.ResponseWriter, r *http.Request)  {
 	w.Header().Set("Content-Type", "text/html")
-	//fmt.Fprint(w, r.URL.Path)
+
 	if r.URL.Path == "/"{
 		fmt.Println("Some one looked")
 		fmt.Fprintf(w, "<h1>Welcome Daju, Sanchai ho</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprintf(w, "To get in touch please send email to <a href=\"mailto:atombombmount@gmail.com\">atombombmount@gmail.com</a>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "<h1>Page Not Found 404 :(</h1>")
 	}
 
 
