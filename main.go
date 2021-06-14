0cc62ab599a0ff82f0b30d69f0171ec115428fb1
tommt <atombombmount@gmail.com>
2021-06-14 15:55:57 +0545

    set Header

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import "fmt"
 import "net/http"
 
 func handlerFunc(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Println("Some one looked")
 	fmt.Fprintf(w, "<h1>Welcome Daju, Sanchai ho</h1>")
 }
 
