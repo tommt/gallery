c571328ac7c6999df5db992d29a81ec7e125541f
tommt <atombombmount@gmail.com>
2021-06-14 18:16:35 +0545

    FAQ page

--- main.go
+++ main.go
@@ -17,10 +17,15 @@ func contact(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "To get in touch please send email to <a href=\"mailto:atombombmount@gmail.com\">atombombmount@gmail.com</a>")
 }
 
+func faq(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "<h1>Frequently Asked Questions!</h1>")
+}
 
 func main(){
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
+	r.HandleFunc("/faq", faq)
 	r.HandleFunc("/contact", contact)
 	http.ListenAndServe(":3000", r)
 }
