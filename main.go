895e4b87d6ebec41057d6ff405ddbde059f5b2ff
tommt <atombombmount@gmail.com>
2021-06-14 18:02:14 +0545

    implemented gotilla/mux router

--- main.go
+++ main.go
@@ -6,25 +6,21 @@ import (
 )
 import "net/http"
 
-func handlerFunc(w http.ResponseWriter, r *http.Request)  {
+
+func home(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "<h1>Welcome to my awesome site!</h1>")
+}
 
-	if r.URL.Path == "/"{
-		fmt.Println("Some one looked")
-		fmt.Fprintf(w, "<h1>Welcome Daju, Sanchai ho</h1>")
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprintf(w, "To get in touch please send email to <a href=\"mailto:atombombmount@gmail.com\">atombombmount@gmail.com</a>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "<h1>Page Not Found 404 :(</h1>")
-	}
-
-
+func contact(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "To get in touch please send email to <a href=\"mailto:atombombmount@gmail.com\">atombombmount@gmail.com</a>")
 }
 
 
 func main(){
 	r := mux.NewRouter()
-	r.HandleFunc("/", handlerFunc)
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
 	http.ListenAndServe(":3000", r)
 }
