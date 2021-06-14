f0ed6af6076dfc8a475a643024220f2e0dc8ce5e
tommt <atombombmount@gmail.com>
2021-06-14 15:33:25 +0545

    create server

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import "fmt"
+import "net/http"
 
-func main()  {
-	fmt.Println("Hello World")
+func handlerFunc(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprintf(w, "<h1>Welcome Daju, Sanchai ho</h1>")
+}
+
+func main(){
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
 }
