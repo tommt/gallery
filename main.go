9658cf05ef5a7d21fc0c5ba064f3f0ce27b7ee00
tommt <atombombmount@gmail.com>
2021-06-14 18:28:10 +0545

    custom 404 page

--- main.go
+++ main.go
@@ -22,8 +22,16 @@ func faq(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "<h1>Frequently Asked Questions!</h1>")
 }
 
+func notFound(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(w, "<h1>Sorry! Page not Found...</h1>")
+}
+
+
 func main(){
 	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(notFound)
 	r.HandleFunc("/", home)
 	r.HandleFunc("/faq", faq)
 	r.HandleFunc("/contact", contact)
