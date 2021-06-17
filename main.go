a3faf75c9e7ef4bcdacd78f3d62b3e70cb237f0e
tommt <atombombmount@gmail.com>
2021-06-17 18:49:03 +0545

    add bulma templates

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 var (
 	homeView *views.View
 	contactView *views.View
+	signupView *views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request){
@@ -22,13 +23,20 @@ func contact(w http.ResponseWriter, r *http.Request){
 	must(contactView.Render(w, nil))
 }
 
+func signup(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "text/html")
+	must(signupView.Render(w, nil))
+}
+
 
 func main(){
 	homeView = views.NewView("bulma","views/home.gohtml")
 	contactView = views.NewView("bulma", "views/contact.gohtml")
+	signupView = views.NewView("bulma", "views/signup.gohtml")
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/signup", signup)
 
 	http.ListenAndServe(":3000", r)
 }
